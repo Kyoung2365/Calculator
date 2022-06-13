ee94746d335ff8a68ce6eaf12cb63617e1ba39b0
Kyle Young <KEYoung@student.fullsail.edu>
2022-06-13 18:26:24 -0400

    Stuff Gone

--- Calculator/Screen.h
+++ Calculator/Screen.h
@@ -1,53 +1,27 @@
 #pragma once
 #include "wx/wx.h"
+#include <vector>
+#include <string>
 
 class Screen : public wxFrame
 {
-
 private:
-	wxTextCtrl* Display = nullptr;
-
+	wxTextCtrl* Display;
 	std::vector<wxButton*> numButtons;
-	wxButton* hexButton = nullptr;
-	wxButton* binButton = nullptr;
-	wxButton* decButton = nullptr;
-	wxButton* modButton = nullptr;
-	wxButton* deciButton = nullptr;
-	wxButton* signButton = nullptr;
-	wxButton* addButton = nullptr;
-	wxButton* subButton = nullptr;
-	wxButton* multButton = nullptr;
-	wxButton* divButton = nullptr;
-	wxButton* clsButton = nullptr;
-	wxButton* equalsButton = nullptr;
-
-	std::string preDecimal;
-	std::string postDecimal;
-	bool decimal = false;
-	bool positive = true;
-	bool entryMode = true;
-	double total = 0;
-	double last = 0;
-	int op = -1;
-
+	wxButton* hexButton;
+	wxButton* binButton;
+	wxButton* decButton;
+	wxButton* modButton;
+	wxButton* deciButton;
+	wxButton* signButton;
+	wxButton* addButton;
+	wxButton* subButton;
+	wxButton* multButton;
+	wxButton* divButton;
+	wxButton* clsButton;
+	wxButton* equalsButton;
 public:
 	Screen();
 	~Screen();
-	
-	/*template<int NUM>*/
-	void OnButtonClick(wxCommandEvent& evt);
-
-	void Clear();
-	/*void ClearAndStore();*/
-
-	template<int OP>
-	void SignButtonClick(wxCommandEvent&);
-
-	void DisplayUpdate();
-	/*double getCurrentValue();
-	void Total();
-	void PerformOp();*/
-
-	wxDECLARE_EVENT_TABLE();
 };
 
